357365959154f920181f14d0234b7cdc8cdd0474
j <joelsouthgate@googlemail.com>
2019-10-23 18:01:13 +0100

    testing launcher

--- distlink/__main__.py
+++ distlink/__main__.py
@@ -1,24 +1,36 @@
 import argparse
+import distlink.globalserver.globalserver as gs
+import distlink.worker.worker as wo
 
 parser = argparse.ArgumentParser()
 parser.add_argument("mode")
 parser.add_argument("-nMachine", type=int)
 parser.add_argument("-globalHostName", type=str)
-parser.add_argument("-inputFiles", type=str, default=None)
-parser.add_argument("-outDirs", type=str, default=None)
+parser.add_argument("-inputFiles", nargs='+', type=str, default=None)
+parser.add_argument("-outDirs", nargs='+', type=str, default=None)
 
 args = parser.parse_args()
 
-if args.mode == "server" and (args.inputFiles is None or args.outDirs is None):
-    parser.error("server requires both -inputFiles and -outDirs to be specified")
 if args.mode == "server":
-    import distlink
-    import distlink.worker
-    import distlink.globalserver.globalserver as gs
+    if (args.inputFiles is None or args.outDirs is None):
+        parser.error("Server requires both -inputFiles and -outDirs to be specified")
     gs.run_server(args.nMachine, args.globalHostName, args.inputFiles, args.outDirs)
+
 elif args.mode == "worker":
-    import distlink.worker.worker as wo
     wo.run_worker(args.nMachine, args.globalHostName)
+
+elif args.mode == "test":
+    import distlink.tests.generate_dataset
+    import distlinktests.scipy_cluster
+    if args.outDirs is None:
+        parser.error("Integration test requires -outDirs to be specified")
+    randomFastaFname = "%s/test.fasta" % args.outDirs
+    write_random_fasta(randomFastaFname, 1000, 10000) 
+    Zval = scipy_cluster.validation_cluster(randomFastaFname)
+    Z = gs.run_server(args.nMachine, args.globalHostName, randomFasta, args.outDirs)
+    assert Zval[:,:2] == Z[:,:2]
+
+    
     
 
 
