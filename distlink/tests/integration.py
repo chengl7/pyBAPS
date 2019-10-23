308513665617b5c917c9e61fe9ec4c3c0356e90f
j <joelsouthgate@googlemail.com>
2019-10-23 11:15:05 +0100

    setup tests

---
+++ distlink/tests/integration.py
@@ -0,0 +1,16 @@
+from scipy.cluster.hierarchy import linkage
+from scipy.spatial.distance import hamming
+import numpy as np
+import random
+
+for i in range(100):
+    n = random.randint(100,1000)
+    d = random.randint(100,1000)
+    X=np.random.randint(0,2,(n,d),dtype='uint8')
+    print("n=%d,d=%d" %(n,d))
+    Z1 = linkage(X,method='complete',metric='hamming')
+    print(Z1)
+
+print("tests passed! happy clustering")
+
+
