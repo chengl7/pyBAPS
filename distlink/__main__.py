12ee97cd01c45ce73598c172bf5d745e375289a0
j <joelsouthgate@googlemail.com>
2019-10-23 11:25:03 +0100

    call to module main

--- distlink/__main__.py
+++ distlink/__main__.py
@@ -2,7 +2,7 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("mode")
-parser.add_argument("-N", type=int)
+parser.add_argument("-nMachine", type=int)
 parser.add_argument("-globalHostName", type=int)
 parser.add_argument("-inputFiles", type=int, default=None)
 parser.add_argument("-outDirs", type=int, default=None)
@@ -14,10 +14,10 @@ if args.mode == "server" and (args.inputFiles is None or args.outDirs is None):
 
 if args.mode == "server":
     import globalserver
-    globalserver.run_server(args.N, args.globalHostName, args.inputFiles, args.outDirs)
+    globalserver.run_server(args.nMachine, args.globalHostName, args.inputFiles, args.outDirs)
 elif args.mode == "worker":
     import worker
-    worker.run_worker(args.N, args.globalHostName)
+    worker.run_worker(args.nMachine, args.globalHostName)
     
 
 
